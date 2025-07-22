f5c8e17bca4dd6b7482055830b4d9fd7044044bd
József Sallai <jozsef@sallai.me>
2025-07-22 19:42:03 +0300

    add UE60

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,6 @@
 import type { Student } from "@prisma/client";
 
 export type StarLevel = 1 | 2 | 3 | 4 | 5;
-export type UELevel = 1 | 2 | 3;
+export type UELevel = 1 | 2 | 3 | 4;
 
 export type { Student };
