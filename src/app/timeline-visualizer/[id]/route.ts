d0fdb5258b2ede41a534fb86bce4401c5fa91da9
József Sallai <jozsef@sallai.me>
2025-10-05 18:43:30 +0300

    oops

--- src/app/timeline-visualizer/[id]/route.ts
+++ src/app/timeline-visualizer/[id]/route.ts
@@ -1,10 +1,13 @@
 import { redirect } from "next/navigation";
 
-export async function GET({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+export async function GET(
+  _req: Request,
+  {
+    params,
+  }: {
+    params: Promise<{ id: string }>;
+  },
+) {
   const { id } = await params;
   return redirect(`/timelines/${id}`);
 }
