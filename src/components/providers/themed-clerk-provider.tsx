64f4db98d8e112b503f1e787c4a7b41a22cf2939
József Sallai <jozsef@sallai.me>
2026-06-27 11:46:55 +0300

    update clerk

--- src/components/providers/themed-clerk-provider.tsx
+++ src/components/providers/themed-clerk-provider.tsx
@@ -24,7 +24,7 @@ export function ThemedClerkProvider({ children }: PropsWithChildren) {
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: resolvedTheme === "dark" ? clerkDark : undefined,
+        theme: resolvedTheme === "dark" ? clerkDark : undefined,
       }}
       localization={clerkLocale}
     >
