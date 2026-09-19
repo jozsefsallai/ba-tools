8e87b2b3e60ff06c2d61abea77a35e263ed62a47
József Sallai <jozsef@sallai.me>
2026-09-19 15:30:20 +0300

    add crossorigin attribute to screenshot method

--- src/lib/screenshot.ts
+++ src/lib/screenshot.ts
@@ -14,6 +14,7 @@ export function prepareCdnImagesForScreenshot(document: Document) {
       continue;
     }
 
+    image.crossOrigin = "anonymous";
     imageUrl.searchParams.set("cors", SCREENSHOT_CORS_QUERY);
     image.src = imageUrl.toString();
   }
